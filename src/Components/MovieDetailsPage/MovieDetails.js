bd7259eabf578686069e509cebc8fd51033dd86b
annaliarosed <56635645+annaliarosed@users.noreply.github.com>
2020-09-16 13:26:14 +0300

    was overcomplicating id, fixed rerenderings and simplified code

--- src/Components/MovieDetailsPage/MovieDetails.js
+++ src/Components/MovieDetailsPage/MovieDetails.js
@@ -1,30 +1,14 @@
 import React from "react";
-import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import "./moviedetailspage.css";
 import MovieDetailsBanner from "./MovieDetailsBanner";
 
-const MovieDetails = ({ movies, APIKEY }) => {
+const MovieDetails = ({ APIKEY }) => {
   const { movieId } = useParams();
-  const [currentMovie, setCurrentMovie] = useState({});
-  const num = parseFloat(movieId);
-  const currentMovieId = movies.find((movie) => movie.id === num);
-//I think the problem is with this currentMovieId section, component rerenders too many times as well I think
-  useEffect(() => {
-    setCurrentMovie(currentMovieId);
-  }, []);
 
   return (
     <>
-      <MovieDetailsBanner
-        id={currentMovie.id}
-        APIKEY={APIKEY}
-        backgroundImg={currentMovie.backdrop_path}
-        poster={currentMovie.poster_path}
-        title={currentMovie.original_title}
-        releaseDate={currentMovie.release_date}
-        overview={currentMovie.overview}
-      />
+      <MovieDetailsBanner id={movieId} APIKEY={APIKEY} />
     </>
   );
 };

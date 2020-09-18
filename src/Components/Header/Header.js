7ce0b60d85d2afadc78f90c513faacdefecac33f
annaliarosed <56635645+annaliarosed@users.noreply.github.com>
2020-09-18 14:48:39 +0300

    refresh page on header click

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,9 +3,13 @@ import "./header.css";
 import { Link } from "react-router-dom";
 
 const Header = (props) => {
+  const handleClick = () => {
+    window.location.reload(true);
+  };
+
   return (
     <header className="header-container">
-      <Link to="/">
+      <Link to="/" onClick={handleClick}>
         <h1>{props.title}</h1>
       </Link>
     </header>

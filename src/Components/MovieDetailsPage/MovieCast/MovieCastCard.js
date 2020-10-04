377215304dd43552c85f05d426d66f876185996f
annaliarosed <56635645+annaliarosed@users.noreply.github.com>
2020-10-04 17:26:49 +0300

    updated styles on moviecast section

--- src/Components/MovieDetailsPage/MovieCast/MovieCastCard.js
+++ src/Components/MovieDetailsPage/MovieCast/MovieCastCard.js
@@ -18,25 +18,23 @@ const MovieCastCard = ({ image, actorName, characterName, APIKEY }) => {
   console.log(people.id);
 
   return (
-    <div className="cast-card">
-      <a
-        href={`https://www.themoviedb.org/person/${people.id}`}
-        className="actor-link"
-      >
-        {image ? (
-          <img
-            src={`https://image.tmdb.org/t/p/w185${image}`}
-            alt="movie poster"
-          />
-        ) : (
-          <p className="no-cast-photo">no image available</p>
-        )}
-        <div className="name-container">
-          <h3 className="actor">{actorName}</h3>
-          <p className="character">{characterName}</p>
-        </div>
-      </a>
-    </div>
+    <a
+      href={`https://www.themoviedb.org/person/${people.id}`}
+      className="cast-card"
+    >
+      {image ? (
+        <img
+          src={`https://image.tmdb.org/t/p/w185${image}`}
+          alt="movie poster"
+        />
+      ) : (
+        <p className="no-cast-photo">no image available</p>
+      )}
+      <div className="name-container">
+        <h3 className="actor">{actorName}</h3>
+        <p className="character">{characterName}</p>
+      </div>
+    </a>
   );
 };
 

4b28835dc0bcd51bdc6158cdf64c4cf24f6a502f
annaliarosed <56635645+annaliarosed@users.noreply.github.com>
2020-10-04 15:26:27 +0300

    Updated searchbar css

--- src/Components/MainPage/SearchBar/SearchBar.js
+++ src/Components/MainPage/SearchBar/SearchBar.js
@@ -7,28 +7,26 @@ const SearchBar = ({ caption, handleSubmit, searchValue, setSearchValue }) => {
   };
 
   return (
-    <div className="input-container-wrapper">
-      <div className="input-container">
-        <form className="form-container" onSubmit={handleSubmit}>
-          <input
-            placeholder="Search..."
-            className="search-bar"
-            type="text"
-            value={searchValue}
-            onChange={handleChange}
-          />
-          <img
-            onClick={handleSubmit}
-            type="submit"
-            className="search-logo"
-            alt="search logo"
-            src="https://img.icons8.com/windows/32/000000/search.png"
-          />
-        </form>
-        <h6 className="caption">
-          Millions of movies, TV shows and people to discover. Explore now.
-        </h6>
-      </div>
+    <div className="input-container">
+      <form className="form-container" onSubmit={handleSubmit}>
+        <input
+          placeholder="Search..."
+          className="search-bar"
+          type="text"
+          value={searchValue}
+          onChange={handleChange}
+        />
+        <img
+          onClick={handleSubmit}
+          type="submit"
+          className="search-logo"
+          alt="search logo"
+          src="https://img.icons8.com/windows/32/000000/search.png"
+        />
+      </form>
+      <h6 className="caption">
+        Millions of movies, TV shows and people to discover. Explore now.
+      </h6>
     </div>
   );
 };
